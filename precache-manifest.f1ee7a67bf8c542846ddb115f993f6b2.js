b7d8adbd6b07778b932e053a2395bebfc406c09b
luiggidev <65421097+luiggidev@users.noreply.github.com>
2021-11-04 14:56:14 +0000

    Deploying to gh-pages from  @ ccf178f93fd4cd6dc3cc987859fb1a253c5ffd35 🚀

---
+++ precache-manifest.f1ee7a67bf8c542846ddb115f993f6b2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25712b99cbd7c5d8eea1bd1dd6ef4a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a584ee8e3efb9bbb53f4",
+    "url": "/static/css/3.f2344a45.chunk.css"
+  },
+  {
+    "revision": "d696ba9697bdc38601cf",
+    "url": "/static/css/4.f62bcc13.chunk.css"
+  },
+  {
+    "revision": "4da44e0c0bb099ead589",
+    "url": "/static/css/main.61db25be.chunk.css"
+  },
+  {
+    "revision": "c2575d6a55cbf95b7d5a",
+    "url": "/static/js/2.7b8002bb.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.7b8002bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a584ee8e3efb9bbb53f4",
+    "url": "/static/js/3.c5375608.chunk.js"
+  },
+  {
+    "revision": "d696ba9697bdc38601cf",
+    "url": "/static/js/4.afe8fb20.chunk.js"
+  },
+  {
+    "revision": "4da44e0c0bb099ead589",
+    "url": "/static/js/main.d4a4d62d.chunk.js"
+  },
+  {
+    "revision": "84e3ef9b0b305164936e",
+    "url": "/static/js/runtime-main.663f7860.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "f6c1de992fcb2224a73cb0fb37d8bf63",
+    "url": "/static/media/FluidwebLogo.f6c1de99.jpg"
+  },
+  {
+    "revision": "8542ddebeea75a06f06eaab09ce57d09",
+    "url": "/static/media/JavaScriptAlgorithms.8542ddeb.png"
+  },
+  {
+    "revision": "358c6e6b76963649a624a8102217370f",
+    "url": "/static/media/LobsterAnywhere.358c6e6b.jpg"
+  },
+  {
+    "revision": "cdfd3271cdf7e861dc4fdb5b6f99d5f8",
+    "url": "/static/media/Logo_Univali.cdfd3271.png"
+  },
+  {
+    "revision": "49945915cb1e3278305bb8571bbb01b3",
+    "url": "/static/media/Luiggi-dev-b.49945915.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b7d33803a1111844124ccbd2feb2d728",
+    "url": "/static/media/Tattoo-bon.b7d33803.png"
+  },
+  {
+    "revision": "9761cc00eb1b0f32d0463bcbf82b63ee",
+    "url": "/static/media/VanillaJSAcademy.9761cc00.png"
+  },
+  {
+    "revision": "4a3b08a332d0f040260fca55f9a97a94",
+    "url": "/static/media/WPdevelopment.4a3b08a3.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f780bf13987e5c430fcb98770bba0688",
+    "url": "/static/media/eixo_snetor_brasil2.f780bf13.jpg"
+  },
+  {
+    "revision": "89ee2e2d82c392161cc6d0ac6c97d5b4",
+    "url": "/static/media/luiggi.89ee2e2d.png"
+  },
+  {
+    "revision": "0250f7761f333e08d942938c8f80992b",
+    "url": "/static/media/racineJewels.0250f776.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "fe6ed45cc2c9d9f9b6b7e904ba30e48a",
+    "url": "/static/media/vertical_sigla_fundo_claro.fe6ed45c.png"
+  }
+]);
