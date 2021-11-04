2c3d462c5efc6fe3625c20f47e2c1c244fa6b496
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2021-11-04 12:09:59 -0300

    Removes unused openSource var

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,8 +6,7 @@ import StyleContext from "../../contexts/StyleContext";
 import {
   greeting,
   workExperiences,
-  skillsSection,
-  openSource,
+  skillsSection,  
   blogSection,
   talkSection,
   achievementSection
@@ -16,7 +15,6 @@ import {
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
-  const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
   const viewBlog = blogSection.display;
